e15b56cadbed3f46b808f7eb4f96e863f8e20dc5
Hussein Nyakigera <qurdyhussein@gmail.com>
2025-09-12 01:41:27 +0300

    Configure PostgreSQL connection and auto-migrate via render.yaml

--- config/settings.py
+++ config/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 
 import os
+import dj_database_url
+
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -82,10 +85,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
+
 }
 
 
